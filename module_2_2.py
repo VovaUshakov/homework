16cb6f351453d71796034127842386616cdfbe9c
VovaUshakov <vova06002@yandex.ru>
2024-10-02 01:01:09 +0700

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -2,9 +2,9 @@ first = int (input('Введите первое число '))
 second = int (input('Введите второе  число '))
 third  = int (input('Введите третье число '))
 
-if first==second==third :
+if first == second == third :
     print( 3)
-elif first==second or second==third or first==third :
+elif first == second or second == third or first == third :
     print(2)
 else:
     print(0)
