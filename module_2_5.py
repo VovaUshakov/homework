a173de32db6b6d3a3ef349b761916cbdea931443
VovaUshakov <vova06002@yandex.ru>
2024-10-08 17:10:31 +0700

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,13 @@
+def get_matrix(n, m, value):
+    matrix=[]
+    for i in range(n):
+        lines = []
+        matrix.append(lines)
+        for k in range(m):
+            lines.append(value)
+    print(matrix)
+    matrix=[]
+    return matrix
+
+
+matrix = get_matrix(3, 2, 1)
