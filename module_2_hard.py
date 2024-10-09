239381fbf232543dba4124a1b6c33b13a13b8869
VovaUshakov <vova06002@yandex.ru>
2024-10-09 19:47:48 +0700

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,7 @@
+x = int(input("Скорее , води число из  первой вставки "))
+nambers = ''
+for i in range(1, x):
+    for k in range(i + 1, x):
+        if x % (i + k) == 0:
+            nambers += str(i) + str(k)
+print('Вводи код и беги : ', nambers)
