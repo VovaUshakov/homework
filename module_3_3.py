d4f37acb2c3807387cb1e49024012af562cee1e0
VovaUshakov <vova06002@yandex.ru>
2024-10-14 20:40:40 +0700

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,15 +1,16 @@
-def print_params(a = 1, b = 'строка', c = True) :
-    print(a,b,c)
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
 
-print_params(2,1,'vk')
+
+print_params(2, 1, 'vk')
 print_params()
-print_params(1,3,1)
-print_params(2,'fid')
-print_params(b = 25)
-print_params(c = [1,2,3])
-values_list= [344,'poker',False]
-values_dict = {'a':404,'b':'Reaper','c':[1,2,3]}
+print_params(1, 3, 1)
+print_params(2, 'fid')
+print_params(b=25)
+print_params(c=[1, 2, 3])
+values_list = [344, 'poker', False]
+values_dict = {'a': 404, 'b': 'Reaper', 'c': [1, 2, 3]}
 print_params(*values_list)
 print_params(**values_dict)
-values_list_2= [True,'sun']
-print_params(*values_list_2,42)
+values_list_2 = [True, 'sun']
+print_params(*values_list_2, 42)
