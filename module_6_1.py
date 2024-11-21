8b5c825cc92c1632d8d3c25a6858e31f1b6e0e31
VovaUshakov <vova06002@yandex.ru>
2024-11-21 23:11:15 +0700

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,59 @@
+class Animal :
+    alive = True
+    fed = False
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        if food.edible :
+            self.fed = True
+            return print (f'{self.name} съел {food.name} ')
+        elif not food.edible:
+            self.alive = False
+            return print (f'{self.name} не стал есть {food.name}')
+    pass
+class Plant :
+    edible = False
+    def __init__(self, name):
+        self.name = name
+    pass
+class Mammal (Animal) :
+
+
+
+    pass
+class Predator (Animal) :
+    pass
+class Flower (Plant) :
+    pass
+class Fruit(Plant) :
+    edible = True
+    pass
+
+a1 = Predator('Волк с Уолл-Стрит')
+
+a2 = Mammal('Хатико')
+
+p1 = Flower('Цветик семицветик')
+
+p2 = Fruit('Заводной апельсин')
+
+
+
+print(a1.name)
+
+print(p1.name)
+
+
+
+print(a1.alive)
+
+print(a2.fed)
+
+a1.eat(p1)
+
+a2.eat(p2)
+
+print(a1.alive)
+
+print(a2.fed)
